4ecb148b0296f2ebdafcf3b496ed3568ebf53d03
Naomi <166138356+NaomiEisen@users.noreply.github.com>
2025-11-25 12:58:28 +0200

    update env file

--- setup/example_env.sh
+++ setup/example_env.sh
@@ -1,8 +1,8 @@
 # Replace with your namespace
-export LLMDBENCH_VLLM_COMMON_NAMESPACE=<namespace-name>
-export LLMDBENCH_HARNESS_NAMESPACE=<namespace-name>
+export LLMDBENCH_VLLM_COMMON_NAMESPACE="<namespace-name>"
+export LLMDBENCH_HARNESS_NAMESPACE="<namespace-name>"
 
 # HuggingFace token (or leave blank and use a secret in the cluster)
-export LLMDBENCH_HF_TOKEN=<replace-with-token>
+export LLMDBENCH_HF_TOKEN="<replace-with-token>"
 
 export LLMDBENCH_VLLM_GAIE_CHART_NAME="oci://us-central1-docker.pkg.dev/k8s-staging-images/gateway-api-inference-extension/charts/inferencepool"
