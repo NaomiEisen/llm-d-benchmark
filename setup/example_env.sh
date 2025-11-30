b7c47bb91ebffc0634bc0ba2501e815aae3f059c
Naomi <166138356+NaomiEisen@users.noreply.github.com>
2025-11-30 10:03:35 +0200

    Moved pvc class to env

--- setup/example_env.sh
+++ setup/example_env.sh
@@ -5,4 +5,7 @@ export LLMDBENCH_HARNESS_NAMESPACE="<namespace-name>"
 # HuggingFace token (or leave blank and use a secret in the cluster)
 export LLMDBENCH_HF_TOKEN="<replace-with-token>"
 
+# The default pvc in our cluster will be stuck on pending, use this instead
+export LLMDBENCH_VLLM_COMMON_PVC_STORAGE_CLASS="ocs-storagecluster-cephfs"
+
 export LLMDBENCH_VLLM_GAIE_CHART_NAME="oci://us-central1-docker.pkg.dev/k8s-staging-images/gateway-api-inference-extension/charts/inferencepool"
